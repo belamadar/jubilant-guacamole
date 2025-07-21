deb7163c830b04e02b751ea40461287341c05deb
ConorColwell97 <conormcolwell@gmail.com>
2025-07-21 14:12:41 +0100

    Added json file for getting travel information

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,30 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import Button from "./Components/Button";
 import NavBar from "./Components/NavBar";
 import Travel_Info from "./Components/Travel_Info";
 
 export default function Index() {
-  const styles = StyleSheet.create({
-
-  });
-
+  const travel_data = require('./Travel_Info.json');
+  
   return (
     <ScrollView contentContainerStyle={{ display: "flex", flex: 1, justifyContent: "space-between" }}>
       <View>
+
+        {/*Back button and page title*/}
         <View style={{ display: "flex", flexDirection: "row", padding: 20, gap: "45%" }}>
           <Button icon="arrow-back-sharp" text="" style={0} />
           <Text style={{ fontSize: 32 }}>My Trips</Text>
         </View>
-        <Travel_Info location="Dublin" date="17.08.2025 - 22.08.2025" />
+
+        {/*Display travel data or appropriate message if no data exists*/}
+        {travel_data.length === 0 ? (
+          <Text>No Travel information available</Text>
+        ) : (
+          travel_data.map((item: any, index: any) => (
+            <Travel_Info key={index} location={item.location} date={item.date}/>
+          ))
+        )}
+
       </View>
       <NavBar />
     </ScrollView>
