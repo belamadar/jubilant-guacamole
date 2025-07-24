6e238d17154a2c2259764871f7bc31265d980ad5
ConorColwell97 <conormcolwell@gmail.com>
2025-07-24 21:22:29 +0100

    Input boxes and button added for "New Trip" screen
    Added Input boxes for 'Trip name', 'Start date', 'End date' and
    a button for continuing

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -3,7 +3,7 @@ import { Link, RelativePathString } from 'expo-router';
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type props = {
-    icon: keyof typeof Ionicons.glyphMap,
+    icon: string,
     text: string,
     style: number,
     link: string
@@ -11,41 +11,67 @@ type props = {
 
 export default function Button({ icon, text, style, link }: props) {
     const styles = StyleSheet.create({
-        nav: {
+        textIcon: {
             alignItems: "center",
             gap: 7
         },
 
-        edit: {
+        icon: {
             display: "flex",
             alignItems: "center",
             backgroundColor: "#994c00",
             padding: 8,
             borderRadius: 10
+        },
+
+        text: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#994c00",
+            padding: 8,
+            borderRadius: 10,
+            maxHeight: 50,
+            maxWidth: 75,
+            minWidth: 75,
+            minHeight: 50
         }
+
+
     });
 
     if (style === 0) {
         return (
             <Link href={link as RelativePathString} asChild>
                 <TouchableOpacity>
-                    <View style={styles.nav}>
-                        <Ionicons name={icon} size={32} />
+                    <View style={styles.textIcon}>
+                        <Ionicons name={icon as keyof typeof Ionicons.glyphMap} size={32} />
                         <Text>{text}</Text>
                     </View>
                 </TouchableOpacity>
             </Link>
         );
-    } else {
+    } else if (style === 1) {
         return (
             <Link href={link as RelativePathString} asChild>
                 <TouchableOpacity>
-                    <View style={styles.edit}>
-                        <Ionicons name={icon} color={"white"} size={16}/>
+                    <View style={styles.icon}>
+                        <Ionicons name={icon as keyof typeof Ionicons.glyphMap} color={"white"} size={16} />
                     </View>
                 </TouchableOpacity>
             </Link>
 
         );
+    } else {
+        return (
+            <Link href={link as RelativePathString} asChild>
+                <TouchableOpacity>
+                    <View style={styles.text}>
+                        <Text style={{ color: "white" }}>{text}</Text>
+                    </View>
+                </TouchableOpacity>
+            </Link>
+        );
+
     }
 }
--- app/new-trip.tsx
+++ app/new-trip.tsx
@@ -1,9 +1,57 @@
-import { ScrollView, Text } from "react-native";
+import React from "react";
+import { ScrollView, StyleSheet, TextInput, View } from "react-native";
+import Button from "./Components/Button";
 
 export default function NewTrip() {
+    const [name, setName] = React.useState('');
+    const [startDate, setStartDate] = React.useState('');
+    const [endDate, setEndDate] = React.useState('');
+
+    const styles = StyleSheet.create({
+        constainer: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 50,
+            gap: 20,
+        },
+
+        input: {
+            display: "flex",
+            flex: 1,
+            fontSize: 20,
+            opacity: 0.7,
+            padding: 20,
+            borderWidth: 1,
+            borderRadius: 5,
+            borderColor: "black",
+            minWidth: 300
+        }
+    });
+
     return (
-        <ScrollView contentContainerStyle={{ display: "flex", flex: 1, backgroundColor: "white", alignItems: "center", justifyContent: "center" }}>
-            <Text style={{ fontSize: 64, fontWeight: "bold" }}>New Trip</Text>
+        <ScrollView contentContainerStyle={{ display: "flex", flex: 1, backgroundColor: "white" }}>
+            <View style={styles.constainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setName}
+                    value={name}
+                    placeholder="Trip Name"
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setStartDate}
+                    value={startDate}
+                    placeholder="Start Date: MM/DD/YY"
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setEndDate}
+                    value={endDate}
+                    placeholder="End Date: MM/DD/YY"
+                />
+                <Button text="Next" icon="" style={2} link=""/>
+            </View>
         </ScrollView>
     );
 }
