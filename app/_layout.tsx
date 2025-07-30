0d79d0da510ee2c790682c5e6755eea25edbd86f
ConorColwell97 <conormcolwell@gmail.com>
2025-07-30 22:21:14 +0100

    Minor chnage: Centered screen headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,11 +8,11 @@ export default function RootLayout() {
     <View style={{ display: "flex", flex: 1 }}>
       <View style={{ display: "flex", flex: 1, backgroundColor: "white" }}>
         <Stack>
-          <Stack.Screen name="index" options={{ title: "My Trips" }} />
-          <Stack.Screen name="new-trip" options={{ title: "New Trip" }} />
-          <Stack.Screen name="calendar" options={{ title: "Calendar" }} />
-          <Stack.Screen name="settings" options={{ title: "Settings" }} />
-          <Stack.Screen name="edit-trip" options={{ title: "Edit Trip" }} />
+          <Stack.Screen name="index" options={{ title: "My Trips", headerTitleAlign: "center" }} />
+          <Stack.Screen name="new-trip" options={{ title: "New Trip", headerTitleAlign: "center" }} />
+          <Stack.Screen name="calendar" options={{ title: "Calendar", headerTitleAlign: "center" }} />
+          <Stack.Screen name="settings" options={{ title: "Settings", headerTitleAlign: "center" }} />
+          <Stack.Screen name="edit-trip" options={{ title: "Edit Trip", headerTitleAlign: "center" }} />
         </Stack>
       </View>
       <NavBar />
